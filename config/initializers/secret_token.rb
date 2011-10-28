7ad773181e248b1cb55e701d972dfba73fd6c98c
Krzysztof Walasek <inocenty@poczta.onet.pl>
2011-10-28 14:44:50 +0200

    Zainicjalizowanie projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c261cb2731d8f7448b99b89c7fed31b63b6147ae9ac83202637bb8622d7de8823773e61449bb0bd2a100845e8f27e09f84d6ac5f72eeaa0b700d5daddf07fd19'
